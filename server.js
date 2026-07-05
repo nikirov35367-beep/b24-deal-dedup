f3f229b4314a32c79192d38f25da85ec2bd304ff
nikirov35367-beep <nikirov35367@gmail.com>
2026-07-06 00:51:10 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+{
+  "name": "b24-deal-dedup",
+  "version": "1.0.0",
+  "description": "Bitrix24: поиск дубликатов сделки при создании по телефону/email контакта",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "axios": "^1.7.7"
+  }
+}
